7d2bf9bacc69e1ccad6a8e88e20376aebb0614ce
JavaGeoGroup <Gog1nator>
2019-02-11 18:52:00 +0400

    set configuration

--- src/main/java/ge/ufc/inhouseProjects/service/ProjectService.java
+++ src/main/java/ge/ufc/inhouseProjects/service/ProjectService.java
@@ -1,12 +1,13 @@
 package ge.ufc.inhouseProjects.service;
 
 import ge.ufc.inhouseProjects.entity.Project;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ProjectService {
      List<Project> findAll();
      Project findById(Long id);
+
+
 
 }
