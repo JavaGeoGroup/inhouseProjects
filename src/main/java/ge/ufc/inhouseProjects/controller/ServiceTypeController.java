737a892b0756f06269e181cd53fd04ee28a8bb8e
JavaGeoGroup <Gog1nator>
2019-02-15 17:14:13 +0400

    set configuration

--- src/main/java/ge/ufc/inhouseProjects/controller/ServiceTypeController.java
+++ src/main/java/ge/ufc/inhouseProjects/controller/ServiceTypeController.java
@@ -1,6 +1,5 @@
 package ge.ufc.inhouseProjects.controller;
 
-import ge.ufc.inhouseProjects.configuration.WebSecurityConfig;
 import ge.ufc.inhouseProjects.dao.ServiceTypeDao;
 import ge.ufc.inhouseProjects.entity.ServiceType;
 import ge.ufc.inhouseProjects.service.ServiceTypeService;
@@ -9,7 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ServiceTypeController {
@@ -19,15 +19,12 @@ public class ServiceTypeController {
     @Autowired
     private ServiceTypeService service;
 
-    //@Autowired
-    //private WebSecurityConfig securityConfig;
 
     @GetMapping(value = {"/", "/welcome"})
     public String getServices(Model model) {
         model.addAttribute("title", "welcome");
         model.addAttribute("message", "This is welcome page");
         ServiceType type = typeDao.findById(1L).get();
-        //ServiceType type1 = service.findServiceTypeByName("SOAP");
         Iterable<ServiceType> iterable = typeDao.findAll();
         List<ServiceType> types = new ArrayList<>();
         iterable.forEach(types::add);
@@ -38,7 +35,6 @@ public class ServiceTypeController {
     @GetMapping(value = "/login")
     public String loginPage(Model model) {
         model.addAttribute("title", "loginPage");
-        //typeDao.findServiceTypeById(1);
         return "loginPage";
     }
 }
--- src/main/java/ge/ufc/inhouseProjects/dao/ServiceTypeDao.java
+++ src/main/java/ge/ufc/inhouseProjects/dao/ServiceTypeDao.java
@@ -3,15 +3,9 @@ package ge.ufc.inhouseProjects.dao;
 import ge.ufc.inhouseProjects.entity.ServiceType;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 public interface ServiceTypeDao extends CrudRepository<ServiceType,Long> {
 
-
-    //ServiceType findServiceTypeById(int id);
     @Query("SELECT u FROM ServiceType u WHERE u.name = ?1")
     ServiceType findServiceTypeByName(String name);
 }
